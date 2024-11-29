fb38bfe90c2e24e57b80c0010e411eaa8533672f
LilianBernot <lilian.bernot@gmail.com>
2024-11-29 13:15:16 +0100

    feat(todo): create view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serialisers import TodoSelializer
+from .models import Todo
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSelializer
+    queryset = Todo.objects.all()
